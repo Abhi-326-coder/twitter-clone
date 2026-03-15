f6a74988d66e8068f682ee2ce264a5975e0ff48d
Abhishek <abisheksavalgi326@gmail.com>
2026-03-15 10:54:25 +0530

    add the SignUp pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage.jsx";
+import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
+import LoginPage from "./pages/auth/login/LoginPage.jsx";
 
-  return (
-      <>
-      <h1 className='bg-zinc-500 text-red-500 inline'>hello gouys how are youI am </h1>
-      </>
-  )
+function App() {
+    return (
+            <div className='flex max-w-6xl mx-auto'>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/signup' element={<SignUpPage />} />
+                    <Route path='/login' element={<LoginPage />} />
+                </Routes>
+            </div>
+    );
 }
 
 export default App;
